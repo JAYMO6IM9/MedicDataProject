1c49f1a25d46099cf62ce1b597323bdbb3e055bd
JAYMO6IM9 <126365550+JAYMO6IM9@users.noreply.github.com>
2023-02-25 19:49:40 -0600

    Add files via upload

---
+++ MedicDataProject/account/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from account.models import *
+# Register your models here.
+
+admin.site.register(User),
+admin.site.register(ExpedienteG),
+admin.site.register(ExpedienteD),
+admin.site.register(ExpedienteO),
